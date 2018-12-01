653436a8efdb0a8811f69309fd5b161bb16bb613
prince0203 <mc.prince.0203@gmail.com>
2018-12-01 17:23:06 +0900

    Update RPCGateway.js

--- src/RPCGateway.js
+++ src/RPCGateway.js
@@ -1,15 +1,33 @@
 const DiscordRPC = require('discord-rpc')
+const sleep = require('./sleep')
 
 class RPCGateway {
   constructor (clientId) {
-    this.client = new DiscordRPC.Client({ transport: 'ipc' })
+    this._createClient()
     this.clientId = clientId
   }
 
-  login () {
-    return this.client.login({
-      clientId: this.clientId
-    })
+  _createClient () {
+    this.client = new DiscordRPC.Client({ transport: 'ipc' })
+  }
+
+  async _tryLogin () {
+    try {
+      await this.client.login({
+        clientId: this.clientId
+      })
+    } catch (e) {
+      this._createClient()
+      return false
+    }
+
+    return true
+  }
+
+  async login () {
+    while (!await this._tryLogin()) {
+      await sleep(5 * 1000)
+    }
   }
 
   updateActivity (activity) {
